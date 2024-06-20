f65b1c77f7780d73c19fc4de68ea1453e3325f16
Saket Karn <saketkarn38@gmail.com>
2024-06-21 00:19:31 +0530

    username issue resolved

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,17 +1,23 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "./LogoutButton";
 
 const LoginButton = () => {
-  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
-  console.log("Current User",user)
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-  return( 
+  return (
     <div>
-    {
-      isAuthenticated? <LogoutButton/> : <button onClick={() => loginWithRedirect()}>Log In</button>  
-    }
-    </div>)
+      {isAuthenticated ? (
+        <div>
+          <p>Hello, {user.name}</p>
+          <LogoutButton />
+        </div>
+      ) : (
+        <div>
+          <p>Hello, please login!</p> <button onClick={() => loginWithRedirect()}>Log In</button> </div>
+      )}
+    </div>
+  );
 };
 
 export default LoginButton;
